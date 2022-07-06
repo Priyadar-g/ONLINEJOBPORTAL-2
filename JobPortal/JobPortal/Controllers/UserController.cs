5b0c377f3a191447d5321d17e1cb4e6e3735d466
Priyadar-g <dharshini1905@gmail.com>
2022-07-06 20:10:02 +0530

    New commit to project job portal

---
+++ JobPortal/JobPortal/Controllers/UserController.cs
@@ -0,0 +1,149 @@
+﻿using DatabaseLayer;
+using JobPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class UserController : Controller
+    {
+        private JobshuntDbEntities1 Db = new JobshuntDbEntities1();
+        // GET: User
+        
+        public ActionResult NewUser()
+        {
+            return View(new UserMV());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NewUser(UserMV userMV)
+        {
+            if(ModelState.IsValid)
+            {
+                var checkuser = Db.UserTables.Where(u => u.EmailAddress == userMV.EmailAddress ).FirstOrDefault();
+                if(checkuser != null)
+                {
+                    ModelState.AddModelError("EmailAddress", "Email is Already Registered!");
+                    return View(userMV);
+                }
+
+                 checkuser = Db.UserTables.Where(u => u.UserName == userMV.UserName).FirstOrDefault();
+                if (checkuser != null)
+                {
+                    ModelState.AddModelError("UserName", "Username is Already Registered!");
+                    return View(userMV);
+                }
+                using (var trans = Db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var user = new UserTable();
+                        user.UserName = userMV.UserName;
+                        user.Password = userMV.Password;
+                        user.ContactNo = userMV.ContactNo;
+                        user.EmailAddress = userMV.EmailAddress;
+                        user.UserTypeID = userMV.AreYouProvider == true ? 2 : 3;
+                        Db.UserTables.Add(user);
+                        Db.SaveChanges();
+
+                        if (userMV.AreYouProvider == true)
+                        {
+                            var company = new CompanyTable();
+                            company.UserID = user.UserID;
+                            if(string.IsNullOrEmpty(userMV.Company.EmailAddress))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.EmailAddress", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.CompanyName))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.CompanyName", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.PhoneNo))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.PhoneNo", "Required*");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.Description))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.Description", "Required*");
+                                return View(userMV);
+                            }
+
+
+                            company.EmailAddress = userMV.Company.EmailAddress;
+                            company.CompanyName = userMV.Company.CompanyName;
+                            company.ContactNo = userMV.ContactNo;
+                            company.PhoneNo = userMV.Company.PhoneNo;
+                            company.Logo = "~/Content/assets/img/logo/download.png";
+                            company.Description = userMV.Company.Description;
+                            Db.CompanyTables.Add(company);
+                            Db.SaveChanges();
+                        }
+                        trans.Commit();
+                        return RedirectToAction("Login");
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError(string.Empty, "Please Provide Correct Details!");
+                        trans.Rollback();
+                    }
+                   
+                }
+            }
+            return View(userMV);
+        }
+        public ActionResult Login()
+        {
+            return View(new UserLoginMV());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLoginMV userLoginMV)
+        {
+            if(ModelState.IsValid)
+            {
+                var user=Db.UserTables.Where(u=>u.UserName==userLoginMV.UserName && u.Password==userLoginMV.Password).FirstOrDefault();
+                if(user==null)
+                {
+                    ModelState.AddModelError(string.Empty, "UserName & Password is Incorrect!");
+                    return View(userLoginMV);
+                }
+                Session["UserID"] = user.UserID;
+                Session["UserName"] = user.UserName;
+                Session["UserTypeID"] = user.UserTypeID;
+                if(user.UserTypeID==2)
+                {
+                    Session["CompanyID"] = user.CompanyTables.FirstOrDefault().CompanyID;
+                }
+                return RedirectToAction("Index","Home");
+            }
+                return View(userLoginMV);
+        }
+        public ActionResult Logout()
+        {
+            Session ["UserID"] = string.Empty;
+            Session ["UserName"] = string.Empty;
+            Session ["CompanyID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult AllUsers()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserTypeID"])))
+            {
+                return RedirectToAction("Login", "User");
+            }
+            var users = Db.UserTables.ToList();
+            return View(users);
+        }
+    }
+}
